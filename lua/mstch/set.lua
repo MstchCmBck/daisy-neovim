a3c88ea7bb9be13919ea9ea7e28246abd8e4029a
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-02-20 21:48:41 +0100

    Add line highlighting

--- lua/mstch/set.lua
+++ lua/mstch/set.lua
@@ -1,25 +1,27 @@
 -- Common NVim configuration
 
+-- Status Column config
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- Highlight things ...
 vim.opt.incsearch = true
-
-vim.termguicolors = true
-
-vim.opt.scrolloff = 12
-
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = "Briefly highlight yanked text",
     callback = function() vim.hl.on_yank() end
 })
+
+-- Small nice improvments
+vim.opt.cursorline = true
+vim.termguicolors = true
+vim.opt.scrolloff = 12
 
 -- Require plugins catppucin
 vim.cmd.colorscheme "catppuccin-frappe"
