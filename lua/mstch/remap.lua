05a06f584f79c2a57b6c8a6e689a2399661448a4
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-02-20 17:28:18 +0100

    Add shortcut to fuzzy find git files

--- lua/mstch/remap.lua
+++ lua/mstch/remap.lua
@@ -2,7 +2,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Shortcut for Snacks picker
 vim.keymap.set("n", "<leader>,", Snacks.picker.pickers)
-vim.keymap.set("n", "<C-p>", Snacks.picker.files)
+vim.keymap.set("n", "<leader>pf", Snacks.picker.files)
+vim.keymap.set("n", "<C-p>", Snacks.picker.git_files)
 vim.keymap.set("n", "<leader>/", Snacks.picker.grep)
 vim.keymap.set("n", "<leader>e", Snacks.explorer.open)
 
