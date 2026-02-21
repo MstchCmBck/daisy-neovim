7e4ab68d58718a5f8bee6a17c38efa320bc79f02
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-02-21 17:11:41 +0100

    Introduce which-key

--- lua/mstch/remap.lua
+++ lua/mstch/remap.lua
@@ -1,17 +1,17 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<leader>x", ":.lua<CR>")
-vim.keymap.set("v", "<leader>x", ":lua<CR>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Find file (netrw)" })
+vim.keymap.set("n", "<leader><space>x", "<cmd>source %<CR>", { desc = "Source file" })
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Source line" })
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Source selection" })
 
 -- Shortcut for Snacks picker
-vim.keymap.set("n", "<leader>,", Snacks.picker.pickers)
-vim.keymap.set("n", "<leader>pf", Snacks.picker.files)
-vim.keymap.set("n", "<C-p>", Snacks.picker.git_files)
-vim.keymap.set("n", "<leader>/", Snacks.picker.grep)
-vim.keymap.set("n", "<leader>e", Snacks.explorer.open)
-vim.keymap.set("n", "<leader>gg", Snacks.lazygit.open)
-vim.keymap.set("n", "<leader>b", Snacks.picker.buffers)
+vim.keymap.set("n", "<leader>,", Snacks.picker.pickers, { desc = "Open picker" })
+vim.keymap.set("n", "<leader>pf", Snacks.picker.files, { desc = "Find file" })
+vim.keymap.set("n", "<C-p>", Snacks.picker.git_files, { desc = "Find file (git)" })
+vim.keymap.set("n", "<leader>/", Snacks.picker.grep, { desc = "Grep search" })
+vim.keymap.set("n", "<leader>e", Snacks.explorer.open, { desc = "File Explorer" })
+vim.keymap.set("n", "<leader>g", Snacks.lazygit.open, { desc = "Git" })
+vim.keymap.set("n", "<leader>b", Snacks.picker.buffers, { desc = "Buffer" })
 
 -- Shortcut for Snacks dashboard
-vim.keymap.set("n", "<leader>w", Snacks.dashboard.open)
+vim.keymap.set("n", "<leader>w", Snacks.dashboard.open, { desc = "Welcome"})
 
