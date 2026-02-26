a9522530bc29bacbbcf2cae6f7737e6ef3ab6a5d
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-02-26 19:34:58 +0100

    Fix LSP error while looking at the config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -92,19 +92,25 @@ return {
                 on_init = function(client)
                     if client.workspace_folders then
                         local path = client.workspace_folders[1].name
-                        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+                        if path ~=
+                            vim.fn.stdpath 'config' and
+                            (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+                            return
+                        end
                     end
 
                     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
                         runtime = {
-                            version = 'Lua 5.4',
+                            version = 'LuaJIT',
                             path = { 'lua/?.lua', 'lua/?/init.lua' },
                         },
                         workspace = {
                             checkThirdParty = false,
-                            -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-                            --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-                            library = vim.api.nvim_get_runtime_file('', true),
+                            library = {
+                                vim.env.VIMRUNTIME,
+                                "${3rd}/luv/library",
+                                unpack(vim.api.nvim_get_runtime_file('', true)),
+                            }
                         },
                     })
                 end,
