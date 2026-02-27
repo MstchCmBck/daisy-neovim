f369fe4f588ee07cb4524c63720506828969d7ad
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-02-27 21:52:18 +0100

    Add autocomplete

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { "nvim-mini/mini.surround", version = false, opts = {} },
     { "nvim-mini/mini.ai", version = false, opts = {} },
     { "nvim-mini/mini.completion", version = false, opts = {} },
+    { "nvim-mini/mini.clue", version = false, opts = {} },
     { "folke/which-key.nvim", version = false, opts = {} },
     -- import your plugins
     { import = "plugins" },
