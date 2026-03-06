267570bfc529642ff44b2960cdd80912589a909a
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-06 21:48:21 +0100

    auto enable lsp server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,15 @@
 return {
   {
+    -- Auto enable installed LSP server
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
+
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -11,8 +21,6 @@ return {
     },
 
     config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-
       -- Special Lua Config, as recommended by neovim help docs
       vim.lsp.config('lua_ls', require('plugins.lsp.lua'))
       vim.lsp.enable 'lua_ls'
