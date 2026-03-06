dd53555be26c2b9a98fe3585440ae8e5e8c49331
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-06 18:36:52 +0100

    Add blink capabilities for jdtls

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,9 +1,12 @@
 -- Configuration specific to java
 
+local capabilities = require('blink.cmp').get_lsp_capabilities()
+
 local config = {
   name = "jdtls",
   cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", "gradlew.bat", ".git" }, { upward = true })[1]),
+  capabilities = capabilities,
 }
 
 require("jdtls").start_or_attach(config)
