932ea1e492181b269a635c2153f14de27399cf8c
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-07 10:06:53 +0100

    Debugger for java is working

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,12 +1,22 @@
 -- Configuration specific to java
 
+local jdtls = require("jdtls")
+
 local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+local bundles = {
+  vim.fn.glob("~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true)
+}
 
 local config = {
   name = "jdtls",
   cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", "gradlew.bat", ".git" }, { upward = true })[1]),
   capabilities = capabilities,
+
+  init_options = {
+    bundles = bundles
+  },
 }
 
 require("jdtls").start_or_attach(config)
---
+++ lua/plugins/java.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "mfussenegger/nvim-jdtls",
+    version = false,
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    ft = "java"
+  },
+  {
+    "microsoft/java-debug",
+    ft = "java"
+  }
+}
