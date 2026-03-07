720617b307d45d10c2d3974c7137f5ed3f2ca4ec
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-07 12:47:49 +0100

    Modify java mason plugin so they can work for both Unix like and Windows

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -2,17 +2,20 @@
 
 local jdtls = require("jdtls")
 
+-- This path should works for both Unix like and Windows
+local mason_folder = vim.fn.stdpath("data") .. "/mason"
+
 local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 local bundles = {
-  vim.fn.glob("~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true)
+  vim.fn.glob(mason_folder .. "/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true)
 }
-local extra = vim.fn.glob("~/.local/share/nvim/mason/packages/java-test/extension/server/*", true, true)
+local extra = vim.fn.glob(mason_folder .. "/packages/java-test/extension/server/*", true, true)
 vim.list_extend(bundles, extra)
 
 local config = {
   name = "jdtls",
-  cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
+  cmd = { vim.fn.expand(mason_folder .. "/bin/jdtls") },
   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", "gradlew.bat", ".git" }, { upward = true })[1]),
   capabilities = capabilities,
 
