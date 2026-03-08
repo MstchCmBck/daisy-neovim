4d72d70e836ea48abd54c2acd27c010f049b7c14
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-09 00:42:57 +0100

    Fix source file command

--- lua/mstch/remap.lua
+++ lua/mstch/remap.lua
@@ -3,27 +3,28 @@
 ---@param func string|function The command or lua function to execute
 ---@param dsc string The description for the keymap (used in which-key, etc.)
 ---@param mode? string|string[] The mode(s) for the mapping (default: 'n')
----@param noremap? boolean Whether to use non-recursive mapping (default: nil)
 local map = function(keys, func, dsc, mode, noremap)
   mode = mode or 'n'
-  vim.keymap.set(mode, keys, func, { desc = dsc, noremap = noremap })
+  vim.keymap.set(mode, keys, func, { desc = dsc, silent = true })
 end
 
+-- Those shortcut needs to be usable whithout map function
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", {desc = "Source line" })
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Source selection" })
+
 -- Shortcut that works wiht Vanilla NVim
-map("<leader>pv", vim.cmd.Ex, "Find file (netrw)")
 map("<leader><space>x", "<cmd>source %<CR>", "Source file")
-map("<leader>x", ":.lua<CR>", "Source line")
-map("<leader>x", ":lua<CR>", "Source selection", "v")
+map("<leader>pv", vim.cmd.Ex, "Find file (netrw)")
 map("<C-space>", "<C-x><C-o>", "Auto-complete", "i")
-map("<leader>tt", [[<C-\><C-n>]], "Enter normal mode in terminal", "t", true)
+map("<leader>tt", [[<C-\><C-n>]], "Enter normal mode in terminal", "t")
 
 -- Use Meta key to move lines
 -- Normal mode
-map("<M-Up>", ":m .-2<CR>==", "Move selection up")
-map("<M-Down>", ":m .+1<CR>==", "Move selection down")
+map("<M-Up>", ":m .-2<CR>==", "Move selection up", "n")
+map("<M-Down>", ":m .+1<CR>==", "Move selection down", "n")
 -- Visual mode
-map("<M-Up>", ":m '<-2<CR>gv=gv", "Move selection up")
-map("<M-Down>", ":m '>+1<CR>gv=gv", "Move selection down")
+map("<M-Up>", ":m '<-2<CR>gv=gv", "Move selection up", "v")
+map("<M-Down>", ":m '>+1<CR>gv=gv", "Move selection down", "v")
 
 -- Shortcut that works with plugins
 map("do", require("mini.diff").toggle_overlay, "Show git diff")
