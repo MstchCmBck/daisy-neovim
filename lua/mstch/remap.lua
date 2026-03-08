05557e6bc346de3086baad385603da91e78ac6e2
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-08 22:11:59 +0100

    Add shortcut to move text with meta key

--- lua/mstch/remap.lua
+++ lua/mstch/remap.lua
@@ -6,6 +6,14 @@ vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Source selection" })
 vim.keymap.set("i", "<C-space>", "<C-x><C-o>", { desc = "Auto-complete" })
 vim.keymap.set("t", "<leader>tt", [[<C-\><C-n>]], { noremap = true, desc = "Enter normal mode in terminal" })
 
+-- Use Meta key to move lines
+-- Normal mode
+vim.keymap.set("n", "<M-Up>", ":m .-2<CR>==", { desc = "Move selection up" })
+vim.keymap.set("n", "<M-Down>", ":m .+1<CR>==", { desc = "Move selection down" })
+-- Visual mode
+vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+
 -- Shortcut that works with plugins
 vim.keymap.set("n", "do", require("mini.diff").toggle_overlay, { desc = "Show git diff" })
 
