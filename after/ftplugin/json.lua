839817eedc9264e1c14ba785a7c3d3d9fc74c0d0
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-09 19:22:24 +0100

    Config file indent set to 2

---
+++ after/ftplugin/json.lua
@@ -0,0 +1,9 @@
+-- Specific to json file type
+
+-- Indentation set to 2 (instead of 4)
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
+vim.opt_local.smartindent = true
+
