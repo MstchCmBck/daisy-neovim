a321d6a7d5b6450cc9eaab3f628b0f5eecf974a2
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-15 13:46:44 +0100

    Call start_or_attach() on each java buffer

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -7,16 +7,22 @@ return {
       "neovim/nvim-lspconfig",
       "saghen/blink.cmp",
     },
-    ft = "java",
+    event = "VeryLazy",
     config = function()
       local jdtls = require("jdtls")
 
       local lsp_config = {
-        name = "jdtls",
+        name = "nvim-jdtls",
         cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
       }
 
-      jdtls.start_or_attach(lsp_config)
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "Attach each java buffer to jdtls",
+        pattern = "java",
+        callback = function()
+          jdtls.start_or_attach(lsp_config)
+        end
+      })
     end,
   },
   {
