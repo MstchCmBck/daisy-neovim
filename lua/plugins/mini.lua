9560553e12b098063408e52bb4acc82df5e18641
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-15 22:05:17 +0100

    Makes mini.diff Lazy loaded

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   { "nvim-mini/mini.icons", version = false, opts = {} },
-  { "nvim-mini/mini.diff", version = false, opts = {} },
+  { "nvim-mini/mini.diff", version = false, event = "VeryLazy", opts = {} },
   { "nvim-mini/mini.statusline", version = false, event = "VeryLazy", opts = {} },
   { "nvim-mini/mini.bracketed", version = false, event = "VeryLazy", opts = {} },
   { "nvim-mini/mini.surround", version = false, event = "VeryLazy", opts = {} },
