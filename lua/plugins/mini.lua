33dba677ee810c4dc68b19edcbc788683b3ff26c
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-15 22:02:51 +0100

    Move all mini plugins in their own files

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+  { "nvim-mini/mini.icons", version = false, opts = {} },
+  { "nvim-mini/mini.diff", version = false, opts = {} },
+  { "nvim-mini/mini.statusline", version = false, event = "VeryLazy", opts = {} },
+  { "nvim-mini/mini.bracketed", version = false, event = "VeryLazy", opts = {} },
+  { "nvim-mini/mini.surround", version = false, event = "VeryLazy", opts = {} },
+  { "nvim-mini/mini.ai", version = false, event = "VeryLazy", opts = {} },
+  { "nvim-mini/mini.clue", version = false, event = "VeryLazy", opts = {} },
+  { "nvim-mini/mini.pairs", version = false, event = "VeryLazy", opts = {} },
+}
