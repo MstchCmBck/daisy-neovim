6e2e7f3ef17c7874e387fadf5cfdd01166569739
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-19 23:46:27 +0100

    Add neowiki note taking

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -9,5 +9,18 @@ return {
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
+  },
+  {
+    "echaya/neowiki.nvim",
+    ft = 'markdown',
+    opts = {
+      wiki_dirs = {
+        { name = "Work", path = vim.fn.expand("~/Documents/Notes/Work") },
+        { name = "Personal", path = vim.fn.expand("~/Documents/Notes/Perso") },
+      }
+    },
+    keys = {
+      { "<leader>ww", function() require('neowiki').open_wiki() end, desc = "Open Wiki" },
+    },
   }
 }
