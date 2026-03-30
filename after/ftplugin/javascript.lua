c70f5b02ff5c93e5fa3ed2dd6c06d5ffeb5bf5b1
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-30 23:21:27 +0200

    Add javascript specific config

---
+++ after/ftplugin/javascript.lua
@@ -0,0 +1,8 @@
+-- Specific to kua file type
+
+-- Indentation set to 2 (instead of 4)
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
+vim.opt_local.smartindent = true
