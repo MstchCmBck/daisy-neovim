3b2045d9c4a555b710a87c20cfe72e38a3bb6ed4
mstch <81013341+MstchCmBck@users.noreply.github.com>
2026-03-31 22:21:47 +0200

    Add neotest

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,54 +5,157 @@ return {
       "rcarriga/nvim-dap-ui",
       "nvim-neotest/nvim-nio",
       "williamboman/mason.nvim",
-      "jay-babu/mason-nvim-dap.nvim"
+      "jay-babu/mason-nvim-dap.nvim",
     },
     keys = {
-      { "<F9>", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint", mode = "n" },
-      { "<F5>", function() require("dap").continue() end, desc = "Continue", mode = "n" },
-      { "<S-F5>", function() require("dap").restart() end, desc = "Restart", mode = "n" },
-      { "<F10>", function() require("dap").step_over() end, desc = "Step over", mode = "n" },
-      { "<F11>", function() require("dap").step_into() end, desc = "Step into", mode = "n" },
-      { "<S-F11>", function() require("dap").step_out() end, desc = "Step out", mode = "n" },
-      { "<F4>", function() require("dapui").close() end, desc = "Exit debugger", mode = "n" },
+      {
+        "<F9>",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle breakpoint",
+        mode = "n",
+      },
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+        mode = "n",
+      },
+      {
+        "<S-F5>",
+        function()
+          require("dap").restart()
+        end,
+        desc = "Restart",
+        mode = "n",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step over",
+        mode = "n",
+      },
+      {
+        "<F11>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step into",
+        mode = "n",
+      },
+      {
+        "<S-F11>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step out",
+        mode = "n",
+      },
+      {
+        "<F4>",
+        function()
+          require("dapui").close()
+        end,
+        desc = "Exit debugger",
+        mode = "n",
+      },
     },
-    config = function ()
+    config = function()
       require("mason-nvim-dap").setup()
 
-      local dap = require "dap"
-      local dapui = require "dapui"
+      local dap = require("dap")
+      local dapui = require("dapui")
 
       dapui.setup()
-      dap.listeners.before.attach.dapui_config = function () dapui.open() end
-      dap.listeners.before.launch.dapui_config = function () dapui.open() end
-      dap.listeners.before.event_terminated.dapui_config = function () dapui.close() end
-      dap.listeners.before.event_exited.dapui_config = function () dapui.close() end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
       -- Define the highlight color (Red)
-      vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermfg = 0, fg = '#993939' })
+      vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermfg = 0, fg = "#993939" })
       -- Define the sign (The Big Red Dot)
-      vim.fn.sign_define('DapBreakpoint', {
-        text = '●',
-        texthl = 'DapBreakpoint',
-        linehl = '',
-        numhl = 'DapBreakpoint'
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "●",
+        texthl = "DapBreakpoint",
+        linehl = "",
+        numhl = "DapBreakpoint",
       })
       -- Define the highlight color (Grey)
-      vim.api.nvim_set_hl(0, 'DapBreakpointRejected', { ctermfg = 0, fg = '#808080' })
+      vim.api.nvim_set_hl(0, "DapBreakpointRejected", { ctermfg = 0, fg = "#808080" })
       -- Define the sign (The Big Grey Dot)
-      vim.fn.sign_define('DapBreakpointRejected', {
-        text = '●',
-        texthl = 'DapBreakpointRejected',
-        linehl = '',
-        numhl = 'DapBreakpointRejected'
+      vim.fn.sign_define("DapBreakpointRejected", {
+        text = "●",
+        texthl = "DapBreakpointRejected",
+        linehl = "",
+        numhl = "DapBreakpointRejected",
       })
-    end
+    end,
   },
 
   {
     "leoluz/nvim-dap-go",
     ft = "go",
     dependencies = { "mfussenegger/nvim-dap" },
-    opts = {}
-  }
+    opts = {},
+  },
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-jest",
+    },
+    keys = {
+      {
+        "<leader>tr",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Test Run",
+      },
+      {
+        "<leader>tk",
+        function()
+          require("neotest").output.open()
+        end,
+        desc = "Test Result",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Test Summary",
+      },
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-jest")({
+            jestCommand = "npm test --",
+            jestConfigFile = "custom.jest.config.ts",
+            env = { CI = true },
+            isTestFile = require("neotest-jest.jest-util").defaultIsTestFile,
+          }),
+        },
+      })
+    end,
+  },
 }
